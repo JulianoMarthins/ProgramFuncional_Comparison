22a1c99e609d66f7a1d913f5e37684f55b0de1bb
Julia <Julia@DESKTOP-OP6TKR8>
2024-09-04 23:39:21 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using ProgramaçãoFuncional.Entities;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramaçãoFuncional {
+    internal class Program {
+        static void Main(string[] args) {
+
+            List<Product> lista = new List<Product>();
+
+            lista.Add(new Product("Televisor", 900.00));
+            lista.Add(new Product("NoteBook", 1200.00));
+            lista.Add(new Product("Xbox One", 450.00));
+            lista.Add(new Product("Guitarra Ibanez", 400.00));
+            
+            Comparison<Product> compName = (p1, p2) => p1.Name.ToLower().CompareTo(p2.Name.ToLower());
+
+            lista.Sort(compName);
+
+            Console.WriteLine("Lista ordenada por nome: ");
+            foreach(Product produto in lista) {
+                Console.WriteLine(produto.ToString());
+            }
+
+            Console.WriteLine("\nLista ordenada por preço: ");
+            lista.Sort((p1, p2) => p1.Price.CompareTo(p2.Price));
+
+            foreach(Product produto in lista) {
+                Console.WriteLine(produto.ToString());
+            }
+
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("ProgramaçãoFuncional")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProgramaçãoFuncional")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("7eb499fe-56b8-46ac-90f6-9f8c623abe7a")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
